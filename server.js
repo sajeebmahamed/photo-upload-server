92df21a43ff38d35a610452f12bdac26f8ce7cb7
Sajeeb <sajeebahamed6@gmail.com>
2020-06-06 00:56:29 +0600

    image on local storage working

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
-
+const cors = require('cors');
 const app = express();
+
+app.use(cors())
 app.use(fileUpload());
 
 // Upload Endpint
