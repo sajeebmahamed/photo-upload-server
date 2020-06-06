a484af034a07abb0ca049fc66e6a7963f93c9d01
Sajeeb <sajeebahamed6@gmail.com>
2020-06-06 11:25:05 +0600

    exoress image upload to mongo

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const fileUpload = require("express-fileupload");
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
 const app = express();
+
+app.use(express.static('public'));
 
 app.use(cors())
 app.use(fileUpload());
 
+const uri = process.env.DB_PATH;
+
+let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 // Upload Endpint
 app.post("/upload", (req, res) => {
     if (req.files === null) {
@@ -20,7 +27,24 @@ app.post("/upload", (req, res) => {
             res.status(500).send(err);
         }
 
-        res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+        // res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+        // //demo  ***for testing perpose***
+        client = new MongoClient(uri, { useNewUrlParser: true });
+        client.connect(err => {
+            // const profile = req.body;
+            const collection = client.db("TeachersProfile").collection("images");
+            collection.insertOne({ filePath: `/uploads/${file.name}` }, (err, result) => {
+                if (err) {
+                    console.log(err)
+                    res.status(500).send({ message: err });
+                }
+                else {
+                    res.send(result);
+                    console.log("success", result)
+                }
+            });
+            client.close();
+        });
     });
 });
 
